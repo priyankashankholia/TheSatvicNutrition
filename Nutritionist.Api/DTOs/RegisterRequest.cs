1de969416e118326e85d9aa4e481b51bf36fabce
priyankashankholia <priyankashankholia21@gmail.com>
2026-08-24 01:55:22 +0530

    Create Nutritionist.Api/DTOs/RegisterRequest.cs

---
+++ Nutritionist.Api/DTOs/RegisterRequest.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Nutritionist.Api.DTOs;
+
+public class RegisterRequest
+{
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(200)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(8)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    public string Role { get; set; } = "Client";
+}
