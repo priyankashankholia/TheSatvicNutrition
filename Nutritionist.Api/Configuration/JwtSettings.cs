5f94cd705a2ef6f4f0f7db96da1459b73c50f606
priyankashankholia <priyankashankholia21@gmail.com>
2026-08-24 01:56:10 +0530

    Create Nutritionist.Api/Configuration/JwtSettings.cs

---
+++ Nutritionist.Api/Configuration/JwtSettings.cs
@@ -0,0 +1,10 @@
+namespace Nutritionist.Api.Configuration;
+
+public class JwtSettings
+{
+    public string Issuer { get; set; } = string.Empty;
+
+    public string Audience { get; set; } = string.Empty;
+
+    public string Key { get; set; } = string.Empty;
+}
